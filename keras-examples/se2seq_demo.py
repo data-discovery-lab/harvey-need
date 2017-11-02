bd875b34fcff2b89e6f9ebfedb24d99c870cd2fa
JStuve <stuve.josh@gmail.com>
2017-11-02 14:46:21 -0500

    Simple seq2seq ex

--- keras-examples/se2seq_demo.py
+++ keras-examples/se2seq_demo.py
@@ -1,63 +1,18 @@
-from seq2seq import SimpleSeq2Seq, Seq2Seq, AttentionSeq2Seq
-import numpy as np
-from keras.utils.test_utils import keras_test
+import keras
+from keras.models import Sequential
+from keras.layers.embeddings import Embedding
+from seq2seq.seq2seq import Seq2seq
+from keras.preprocessing import sequence
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+vocab_size = 20000 #number of words
+maxlen = 100 #length of input sequence and output sequence
+embedding_dim = 200 #word embedding size
+hidden_dim = 500 #memory size of seq2seq
 
-input_length = 5
-input_dim = 3
+embedding = Embedding(vocab_size, embedding_dim, input_length=maxlen)
+seq2seq = Seq2seq(input_length=maxlen, input_dim=embedding_dim,hidden_dim=hidden_dim,
+                  output_dim=embedding_dim, output_length=maxlen, batch_size=10, depth=4)
 
-output_length = 3
-output_dim = 4
-
-samples = 100
-hidden_dim = 24
-
-
-@keras_test
-def test_SimpleSeq2Seq():
-    x = np.random.random((samples, input_length, input_dim))
-    y = np.random.random((samples, output_length, output_dim))
-
-    models = []
-    models += [SimpleSeq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim))]
-    models += [SimpleSeq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), depth=2)]
-
-    for model in models:
-        model.compile(loss='mse', optimizer='sgd')
-        model.fit(x, y, nb_epoch=1)
-
-
-@keras_test
-def test_Seq2Seq():
-    x = np.random.random((samples, input_length, input_dim))
-    y = np.random.random((samples, output_length, output_dim))
-
-    models = []
-    models += [Seq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim))]
-    models += [Seq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), peek=True)]
-    models += [Seq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), depth=2)]
-    models += [Seq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), peek=True, depth=2)]
-
-    for model in models:
-        model.compile(loss='mse', optimizer='sgd')
-        model.fit(x, y, epochs=1)
-
-    model = Seq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), peek=True, depth=2, teacher_force=True)
-    model.compile(loss='mse', optimizer='sgd')
-    model.fit([x, y], y, epochs=1)
-    
-@keras_test
-def test_AttentionSeq2Seq():
-    x = np.random.random((samples, input_length, input_dim))
-    y = np.random.random((samples, output_length, output_dim))
-
-    models = []
-    models += [AttentionSeq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim))]
-    models += [AttentionSeq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), depth=2)]
-    models += [AttentionSeq2Seq(output_dim=output_dim, hidden_dim=hidden_dim, output_length=output_length, input_shape=(input_length, input_dim), depth=3)]
-
-    for model in models:
-        model.compile(loss='mse', optimizer='sgd')
-        model.fit(x, y, epochs=1)
+model = Sequential()
+model.add(embedding)
+model.add(seq2seq)
