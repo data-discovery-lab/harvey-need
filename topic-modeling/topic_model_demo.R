3187c2eee98ecbbaafa869032cc3f96cf53390ba
JStuve <stuve.josh@gmail.com>
2017-11-16 14:21:48 -0600

    Added custome stop words

--- topic-modeling/topic_model_demo.R
+++ topic-modeling/topic_model_demo.R
@@ -5,21 +5,26 @@ library(magrittr) #Used for %>%
 library(tm)
 library(topicmodels)
 
-data("AssociatedPress")
-a  <-Corpus(DirSource("/home/jstuve/GitProj/harvey-need/topic-modeling/text/"), readerControl = list(language="lat")) #specifies the exact folder where my text file(s) is for analysis with tm.
-summary(a)  #check what went in
+#Stopword File
+stopwords <- read.csv("/home/jstuve/GitProj/harvey-need/topic-modeling/stopwords.csv", header = FALSE)
+stopwords <- as.character(stopwords$V1)
+stopwords <- c(stopwords, stopwords("english"))
 
+#Read tweet file
+a  <-Corpus(DirSource("/home/jstuve/GitProj/harvey-need/topic-modeling/text/"), readerControl = list(language="lat")) #Directory for text file analytics
+
+#Clean the data
 a <- tm_map(a, removeNumbers)
 a <- tm_map(a, removePunctuation)
 a <- tm_map(a, tolower)
-a <- tm_map(a, removeWords, stopwords("english")) # this stopword file is at C:\Users\[username]\Documents\R\win-library\2.13\tm\stopwords
+a <- tm_map(a, removeWords, stopwords)
 a <- tm_map(a, stemDocument, language = "english")
-TFM <-DocumentTermMatrix(a) 
-TFM <- removeSparseTerms(adtm, 0.75)
+TFM <-DocumentTermMatrix(a)   #Create a matrix for LDA functino
+TFM <- removeSparseTerms(TFM, 0.75)  #Reduce the matrix size for term that are below .75
 
 
 # set a seed so that the output of the model is predictable
-ap_lda <- LDA(TFM, k = 2, control = list(seed = 1234))
+ap_lda <- LDA(TFM, k = 3, control = list(seed = 1234))
 ap_lda
  
 ap_topics <- tidy(ap_lda, matrix = "beta")
