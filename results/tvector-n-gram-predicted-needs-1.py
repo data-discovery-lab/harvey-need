bd39ac4c4b7a42808378f026ed6d96fa25dd819f
Hoang Long <litpuvn@gmail.com>
2018-04-08 15:07:33 -0500

    add n-gram conversion

--- results/tvector-n-gram-predicted-needs-1.py
+++ results/tvector-n-gram-predicted-needs-1.py
@@ -1,37 +1,59 @@
-#Convert tweeter text data into vector, by Yong Wu 04/06/2018
-#All input and output files should be .txt format
-#If lenth of vocabulary is not 50, this code should be modified a little bit
-#File names of the input and output files should be edited within this code
+# Convert tweeter text data into vector, by Yong Wu 04/06/2018
+# All input and output files should be .txt format
+# If lenth of vocabulary is not 50, this code should be modified a little bit
+# File names of the input and output files should be edited within this code
 
-vector_file = open('needs-voc.txt') #input vocabulary
-vector_model=[] #setup vocabulary vector
+vector_file = open('needs-voc.txt')  # input vocabulary
+vector_model = []  # setup vocabulary vector
+base_vector = {}
 for line in vector_file:
-    line = line.rstrip() #get rid of newline char at the end of each line
-    vector_model.append(line)
-    
-vecter_output = open('vecter_output.txt', 'w') #output vectors
-data_file = open('half-day-need.txt') #input text data
+    line = line.rstrip()  # get rid of newline char at the end of each line
+    if len(line) > 2:
+        base_vector[line] = len(vector_model)
+        vector_model.append(line)
 
-for line in data_file: #parse each line in the input file
-    line = line.replace("{", " ")
-    line = line.replace("}", " ")
-    line = line.replace(",", " ")
-    line = line.replace(":", " ")
-    print line
-##    line = line.rstrip() #get rid of newline char at the end of each line
-##    split_line = line.split(' ') #get rid of whitespace
-##    vector_out=[]  #initialize output vector of a line of tweeter text data
-##    for index in range(50): #compare words with vocabulary list
-##        if vector_model[index] in split_line:
-##            vector_out.append(1)
-##        else:
-##            vector_out.append(0)
-##    #write output vector into output file
-##    vecter_output.write( str(split_line[0])+': '+str(vector_out)+'\n')
+file_name = 'n-gram-predicted-needs-1.txt'
+file_output = open('output/' + file_name.replace('.txt', '.vec.txt'), 'w')  # output vectors
+data_file = open(file_name)  # input text data
 
-#close input and output files
-vecter_output.close()
+output_line = []
+
+for line in data_file:  # parse each line in the input file
+
+    if not line.startswith('2017'):
+        continue
+
+    output_line = []
+
+    line = line.replace('\n', '')
+    if len(line) < 10:
+        continue
+
+    output_line.append(line[0: line.index('#')-1])
+
+    # initialize a zero vector
+    output_vector = []
+    for i in range(len(vector_model)):
+        output_vector.append('0')
+
+    needs = line[line.index('#') + 1:]
+
+    needs = needs.split(' ')
+    for n in needs:
+        n = n.strip()
+        if len(n) < 2:
+            continue
+        idx = base_vector[n]
+        output_vector[idx] = '1'
+
+    # write output vector into output file
+    output_vector_string = ', '.join(output_vector)
+    output_line.append('[' + output_vector_string + ']')
+    file_output.write(' '.join(output_line) + '\n')
+
+# close input and output files
+file_output.close()
 data_file.close()
 vector_file.close()
 
-    
+
