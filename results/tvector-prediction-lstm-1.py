76c453833579e2623d3f95cda02e72f01a82b563
Hoang Long <litpuvn@gmail.com>
2018-04-08 14:23:57 -0500

    add output for lstm1

--- results/tvector-prediction-lstm-1.py
+++ results/tvector-prediction-lstm-1.py
@@ -1,41 +1,63 @@
-#Convert tweeter text data into vector, by Yong Wu 04/06/2018
-#All input and output files should be .txt format
-#If lenth of vocabulary is not 50, this code should be modified a little bit
-#File names of the input and output files should be edited within this code
+# Convert tweeter text data into vector, by Yong Wu 04/06/2018
+# All input and output files should be .txt format
+# If lenth of vocabulary is not 50, this code should be modified a little bit
+# File names of the input and output files should be edited within this code
 
-vector_file = open('needs-voc.txt') #input vocabulary
-vector_model=[] #setup vocabulary vector
+vector_file = open('needs-voc.txt')  # input vocabulary
+vector_model = []  # setup vocabulary vector
+base_vector = {}
 for line in vector_file:
-    line = line.rstrip() #get rid of newline char at the end of each line
-    vector_model.append(line)
-    
-vecter_output = open('vecter_output.txt', 'w') #output vectors
-data_file = open('prediction-lstm-2.txt') #input text data
+    line = line.rstrip()  # get rid of newline char at the end of each line
+    if len(line) > 2:
+        base_vector[line] = len(vector_model)
+        vector_model.append(line)
 
-count = 0
-for line in data_file: #parse each line in the input file
-    count=count+1
-    line = line.rstrip() #get rid of newline char at the end of each line
-    split_line = line.split(' ') #get rid of whitespace
+file_name = 'prediction-lstm-1.txt'
+file_output = open('output/' + file_name.replace('.txt', '.vec.txt'), 'w')  # output vectors
+data_file = open(file_name)  # input text data
 
-    if count%4==1:
-       vecter_output.write( str(" ".join(split_line[4:9]))+': ')
-    if count%4==3:
-       vector_out=[]  #initialize output vector of a line of tweeter text data
-       for index in range(50): #compare words with vocabulary list
-          if vector_model[index] in split_line:
-             vector_out.append(1)
-          else:
-             vector_out.append(0)
-       vecter_output.write( str(vector_out)+'\n')
+output_content = []
+output_line = []
 
+for line in data_file:  # parse each line in the input file
+    line = line.replace('\n', '')
+    line = line.strip()
 
-    #write output vector into output file
-    #vecter_output.write( str(" ".join(split_line[4:8]))+': '+str(vector_out)+'\n')
+    if len(line) < 10:
+        continue
 
-#close input and output files
-vecter_output.close()
+    if line.startswith('****'):
+        output_line = []
+        startingIndex = line.index('.') + 5
+        endingIndex = len(line) - 7
+        output_line.append(line[startingIndex:endingIndex])
+        continue
+    # initialize a zero vector
+    output_vector = []
+    for i in range(len(vector_model)):
+        output_vector.append('0')
+
+    needs = line
+
+    needs = needs.split(' ')
+    for n in needs:
+        n = n.strip()
+        if len(n) < 2:
+            continue
+        if n not in base_vector:
+            continue
+        idx = base_vector[n]
+        output_vector[idx] = '1'
+
+    # write output vector into output file
+    output_vector_string = ', '.join(output_vector)
+    output_line.append('[' + output_vector_string + ']')
+    my_line = ' '.join(output_line) + '\n'
+    file_output.write(my_line)
+
+# close input and output files
+file_output.close()
 data_file.close()
 vector_file.close()
 
-    
+
