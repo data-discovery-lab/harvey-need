c0931f91c3266044a9479ea66f7c93e232b8c398
Cyrus67 <nickalvarez123@gmail.com>
2018-04-11 18:41:17 -0500

    Visualizer using plotly
    Changed visualizer to use plotly plotting.

--- results/visualizer.py
+++ results/visualizer.py
@@ -5,7 +5,49 @@ from ast import literal_eval
 from sklearn.metrics import jaccard_similarity_score
 import numpy as np
 
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import Scatter, Figure, Layout, Bar
+from plotly import tools
+
+tools.set_credentials_file(username='evanwilson', api_key='XVhzaBtJIMl1bjUZj0zo')
+
+
 from scipy.interpolate import spline
+
+def PlotStuff(dataToPlot, dataName, dataColor):
+
+    #Currently limiting to 2 decimal places to make prettier.
+    dataToPlot = [round(x*100)/100 for x in dataToPlot]
+
+    #Smoothed? Remove and uncomment other lines to return to prior format.
+    #The last value is the smoothing factor. Sweet spot for working hoverover uncertain.
+    xnew = np.linspace(0, len(dataToPlot)-1, len(dataToPlot) * 11)
+    smoothedY = spline(np.arange(len(dataToPlot)), dataToPlot, xnew)
+    smoothedX = spline(np.arange(len(dataToPlot)), np.arange(len(dataToPlot)), xnew)
+    
+    dataPlot = Scatter(
+        x = smoothedX,#np.arange(len(dataToPlot)),
+        y = smoothedY,#dataToPlot,
+        name = dataName,
+        line = dict(
+            color = dataColor,
+            width = 4
+            ),
+        marker = dict(
+            symbol = 'triangle-up',
+            size = 17
+            )
+        )
+    return dataPlot
+
+
+
+
+
+
+
+
 
 true_file = 'output/weather-need-full-day.vec.txt'
 files = {
@@ -23,6 +65,8 @@ f = open(true_file)
 lines = f.readlines()
 
 trueVector = []
+
+plotData = []
 
 for file, color in files.items():
 
@@ -54,15 +98,45 @@ for file, color in files.items():
 
         print('average:', file, np.average(similarity_scores))
         #Nick: Adjustments here ------------------------------------
+        plotData.append(PlotStuff(similarity_scores, file, color))
         #Create smoothing effect.
-        xnew = np.linspace(0, len(similarity_scores)-1, 300)
-        smooth = spline(np.arange(len(similarity_scores)), similarity_scores, xnew)
+        #xnew = np.linspace(0, len(similarity_scores)-1, 100)
+        #smooth = spline(np.arange(len(similarity_scores)), similarity_scores, xnew)
         #Plot the smoothed line...
-        plt.plot(xnew, smooth, color=color)
+        #plt.plot(xnew, smooth, color=color)
         #Plot the original, transparently.
-        plt.plot(similarity_scores, color=color + "22")
+        #plt.plot(similarity_scores, color=color + "22")
 
-plt.title("Accuracy Visualization")
+#plt.title("Accuracy Visualization")
 #plt.yticks(np.arange(0, 1.1, step=0.1))
-plt.show()
+#plt.show()
+
+layout = dict(
+    xaxis = dict(
+        title = 'X Axis Label',
+        ),
+    yaxis = dict(
+        title = 'Accuracy',
+        range = [0, 1]
+        ),
+    legend = dict(
+        #x = 0,
+        #y = 1.0,
+        bgcolor = 'rgba(255, 255, 255, 0)',
+        bordercolor = 'rgb(0, 0, 0)',
+        borderwidth = 1#,
+        #orientation = 'h'
+        )
+    )
+
+
+fig = dict(data=plotData, layout=layout)
+plot(fig, filename='line-graph-test.html')
+
+
+
+
+
+
+
 
